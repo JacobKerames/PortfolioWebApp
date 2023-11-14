3b70e8a72536845974578fd62bcddcf7ab71eda7
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-11-14 15:57:59 -0700

    Implemented command-handling and typing emulation

--- ClientApp/src/components/Terminal.js
+++ ClientApp/src/components/Terminal.js
@@ -9,11 +9,11 @@ const Terminal = () => {
           :.       .:::--=:                                      
           :..:.-.::.   ::                                        
           .:::  -     .:                                         
-         :.           :.                                         
+         :.           :.                 
      :-.::.    .=:  .:                                           
   :.  ...  -. :=                                                 
--  :       .. .::                                                
-  :         -: .:=                                                
+:  :       .. .::                                                
+ :          -: .:=                                                
  ...        :+. .+                           .----:::::--        
  .++        .-.  #                          :---:     -=.        
    .=--:.....-. .-:                        :-==:::   -=          
@@ -22,13 +22,157 @@ const Terminal = () => {
        .:.        ...........     :===**=++=-::::::+.            
          .:.          .....:---=++==+++++++=====+-=:.......      
     `;
+    const [typedArt, setTypedArt] = useState('');
+    const name = "Jacob Kerames - Software Engineer";
+    const [typedName, setTypedName] = useState('');
+    const welcome = "\nType \"help\" for a list of commands.";
+    const [typedWelcome, setTypedWelcome] = useState('');
+
     const navigate = useNavigate();
     const [input, setInput] = useState('');
-    const [outputs, setOutputs] = useState(asciiArt); // Start with an empty string
-    const [currentText, setCurrentText] = useState(''); // Holds the current text to display
-    const welcomeMessage = '\nJacob Kerames - Software Engineer\n\nType "help" for a list of commands.';
+    const [outputs, setOutputs] = useState('');
+    const [typingComplete, setTypingComplete] = useState(false);
+    
     const endOfTerminalRef = useRef(null); // Ref to the end of the terminal
-    const Cursor = () => <span className="terminal-cursor">&nbsp;</span>;
+    const inputRef = useRef(null);
+
+    const commandSections = [
+        {
+            title: '\nPROFILES',
+            commands: [
+                { description: 'Open LinkedIn profile', command: 'linkedin' },
+                { description: 'Open GitHub profile', command: 'github' },
+                // Other project-specific commands...
+            ],
+        },
+        {
+            title: '\nPROJECTS',
+            commands: [
+                { description: 'Open this project\'s GitHub repository', command: 'repo' },
+                { description: 'Run counter Project', command: 'counter' },
+                // Other project-specific commands...
+            ],
+        },
+        {
+            title: '\nTERMINAL',
+            commands: [
+                { description: 'Display help menu', command: 'help' },
+                { description: 'Clear terminal input and output', command: 'clear' },
+                // Other general commands...
+            ],
+        },
+        // More sections as needed...
+    ];
+
+    const CommandSection = ({ section }) => (
+        <>
+            {section.title}
+            {section.commands.map((cmd, index) => (
+                <div key={index} className="command-entry">
+                    <span className="command-name">{cmd.command}</span>
+                    <span className="command-description">{cmd.description}</span>
+                </div>
+            ))}
+        </>
+    );
+
+    // Function to handle command input, modify as needed
+    const handleCommand = (e) => {
+        if (e.key === 'Enter') {
+            // Add the input command to the outputs array
+            setOutputs(outputs => [...outputs, `> ${input}\n`]);
+
+            // Process the command
+            const command = input.trim().toLowerCase();
+
+            switch (command) {
+                // Profile commands
+                case 'linkedin':
+                    setOutputs(outputs => [...outputs, '\nOpening LinkedIn profile in a new tab...']);
+                    setTimeout(() => {
+                        window.open('https://www.linkedin.com/in/jacob-kerames/', '_blank');
+                    }, 1500);
+                    break;
+                case 'github':
+                    setOutputs(outputs => [...outputs, '\nOpening GitHub profile in a new tab...']);
+                    setTimeout(() => {
+                        window.open('https://github.com/JacobKerames', '_blank');
+                    }, 1500);
+                    break;
+
+                // Project commands
+                case 'repo':
+                    setOutputs(outputs => [...outputs, '\nOpening the GitHub repository in a new tab...']);
+                    setTimeout(() => {
+                        window.open('https://github.com/JacobKerames/PortfolioWebApp', '_blank');
+                    }, 1500);
+                    break;
+                case 'counter':
+                    navigate('/counter'); // Navigate to the counter page
+                    break;
+
+                // Terminal commands
+                case 'help':
+                    // Map through each section and create a CommandSection component for each
+                    const helpOutput = commandSections.map((section, index) => (
+                        <CommandSection key={index} section={section} />
+                    ));
+                    // Update your output state to include the helpOutput components
+                    setOutputs(outputs => [...outputs, ...helpOutput]);
+                    break;
+                case 'clear':
+                    setOutputs([]);
+                    break;
+                default:
+                    // If the command is not recognized, show an error message
+                    setOutputs(outputs => [...outputs, `\nCommand not recognized: ${command}`]);
+                    break;
+            }
+
+            // Clear the input field
+            setInput('');
+        }
+    };
+
+    useEffect(() => {
+        // Define the typeOutText inside useEffect to capture the latest state with each render
+        const typeOutText = (setText, text, speed, delay = 0, callback = () => { }) => {
+            let index = 0;
+            let currentText = '';
+
+            const timeoutId = setTimeout(() => {
+                const intervalId = setInterval(() => {
+                    currentText += text.charAt(index);
+                    setText(currentText);
+                    index++;
+
+                    if (index >= text.length) {
+                        clearInterval(intervalId);
+                        callback();
+                    }
+                }, speed);
+            }, delay);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        };
+
+        // Start typing effects
+        const clearTypeOutName = typeOutText(setTypedName, name, 50);
+        let delay = (name.length * 50);
+        const clearTypeOutArt = typeOutText(setTypedArt, asciiArt, 1, delay);
+        delay += (asciiArt.length * 4) + 10;
+        const clearTypeOutWelcome = typeOutText(setTypedWelcome, welcome, 50, delay, () => setTypingComplete(true));
+
+        // Cleanup function
+        return () => {
+            // Clear timeouts set by typeOutText
+            clearTypeOutName();
+            clearTypeOutArt();
+            clearTypeOutWelcome();
+        };
+    }, [name, asciiArt, welcome]); // Ensure these are not undefined
 
     // Scroll to the bottom of the terminal output
     useEffect(() => {
@@ -47,98 +191,44 @@ const Terminal = () => {
         };
     }, []);
 
-    const commandSections = [
-        {
-            title: '\n\nGeneral:',
-            commands: [
-                { description: 'Display help menu', command: 'help' },
-                // Other general commands...
-            ],
-        },
-        {
-            title: '\nProjects:',
-            commands: [
-                { description: 'Counter Project', command: 'counter' },
-                // Other project-specific commands...
-            ],
-        },
-        // More sections as needed...
-    ];
-
-    const CommandSection = ({ section }) => (
-        <>
-            {section.title}
-            {section.commands.map((cmd, index) => (
-                <div key={index} className="command-entry">
-                    <span className="command-description">{cmd.description}</span>
-                    <span className="command-name">{cmd.command}</span>
-                </div>
-            ))}
-        </>
-    );
-
-    // Function to handle command input, modify as needed
-    const handleCommand = (e) => {
-        if (e.key === 'Enter') {
-            // Add the input command to the outputs array
-            setOutputs(outputs => [...outputs, `\n> ${input}`]);
-
-            // Process the command
-            const command = input.trim().toLowerCase();
-
-            switch (command) {
-                case 'help':
-                    // Map through each section and create a CommandSection component for each
-                    const helpOutput = commandSections.map((section, index) => (
-                        <CommandSection key={index} section={section} />
-                    ));
-
-                    // Update your output state to include the helpOutput components
-                    setOutputs(outputs => [...outputs, ...helpOutput]);
-                    break;
-                case 'counter':
-                    navigate('/counter'); // Navigate to the counter page
-                    break;
-                default:
-                    // If the command is not recognized, show an error message
-                    setOutputs(outputs => [...outputs, `\nCommand not recognized: ${command}`]);
-                    break;
-            }
-
-            // Clear the input field
-            setInput('');
-        }
+    // This function focuses the input element
+    const focusInput = () => {
+        inputRef.current && inputRef.current.focus();
     };
 
     useEffect(() => {
-        // Function to simulate the typing effect
-        const typeOutText = (text, index = 0) => {
-            if (index < text.length) {
-                // Append the next character
-                setOutputs(current => current + text[index]);
-                setTimeout(() => typeOutText(text, index + 1), 50); // Adjust typing speed as needed
-            }
+        const terminalContainer = document.querySelector('.terminal'); // Adjust the selector to your terminal container
+        terminalContainer.addEventListener('click', focusInput);
+
+        // Cleanup function
+        return () => {
+            terminalContainer.removeEventListener('click', focusInput);
         };
-        typeOutText(welcomeMessage); // Start the typing effect
     }, []);
 
     return (
         <div className="terminal">
             <div className="terminal-body">
+                <pre className="ascii-art">{typedArt}</pre>
+                <pre className="ascii-art-name">{typedName}</pre>
+                <pre className="typed-welcome">{typedWelcome}</pre>
                 <pre className="terminal-output">{outputs}</pre>
                 <div ref={endOfTerminalRef} />
-                <div className="terminal-input-container">
-                    <span className="terminal-prompt">> </span>
-                    <input
-                        type="text"
-                        className="terminal-input"
-                        value={input}
-                        onChange={e => setInput(e.target.value)}
-                        onKeyDown={handleCommand}
-                        placeholder="Type your command..."
-                        autoFocus
-                    />
-                </div>
+                {typingComplete && (
+                    <div className="terminal-input-container">
+                        <span className="terminal-prompt">> </span>
+                        <input
+                            ref={inputRef}
+                            type="text"
+                            className="terminal-input"
+                            value={input}
+                            onChange={e => setInput(e.target.value)}
+                            onKeyDown={handleCommand}
+                            placeholder="Type your command..."
+                            autoFocus
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
