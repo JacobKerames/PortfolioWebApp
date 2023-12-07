3c7348a4d8725746f6bcc46e7cba3ffcb165a737
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2023-12-07 03:10:04 -0700

    Added removed files/directories

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StockSimContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("StockSimConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        corsBuilder => corsBuilder.WithOrigins("https://localhost:44445")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors("AllowSpecificOrigin");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
