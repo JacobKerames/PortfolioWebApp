47caf8381bedb6d5204548d9acd02cfc2c8be6dc
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2024-01-12 17:21:43 -0700

    Fixed component formatting

--- portfolio-web-app/src/app/page.tsx
+++ portfolio-web-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import TerminalWindow from "./components/TerminalComponents/TerminalWindow";
 
 const Home = () => {
-  return <div className="home-container">
-    <TerminalWindow />
-  </div>;
+  return (
+    <div className="home-container">
+      <TerminalWindow />
+    </div>
+  );
 };
 
 export default Home;
