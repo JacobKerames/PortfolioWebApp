740899f3e35312315b1ae0f7a4953f513cd0ad8c
Jacob Kerames <108101472+JacobKerames@users.noreply.github.com>
2024-01-14 21:24:02 -0700

    Added ref to end of home page so that the terminal input is not hidden by the keyboard

--- portfolio-web-app/src/app/page.tsx
+++ portfolio-web-app/src/app/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useTerminalContext } from "./components/TerminalComponents/TerminalContext";
 import TerminalWindow from "./components/TerminalComponents/TerminalWindow";
 
 const Home = () => {
+  const { terminalOutputs } = useTerminalContext();
+
+  const endOfPageRef = useRef<HTMLDivElement>(null);
+
+  // Auto-scrolling effect to keep the latest terminal output in view.
+  useEffect(() => {
+    if (endOfPageRef.current) {
+      endOfPageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [terminalOutputs]);
+
   return (
-    <div className="home-container">
-      <TerminalWindow />
-    </div>
+    <>
+      <div className="home-container">
+        <TerminalWindow />
+      </div>
+      <div ref={endOfPageRef} />
+    </>
   );
 };
 
